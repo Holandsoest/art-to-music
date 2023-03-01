6f74bb6171ff96e58263772534991d9f67025f44
Josha <joshakuipers@gmail.com>
2023-03-01 21:15:01 +0100

    tutorial

---
+++ source/handythings.py
@@ -0,0 +1,14 @@
+import random
+print(random.randrange(1, 10))
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+#python list where i endend
+#https://www.w3schools.com/python/python_lists.asp
