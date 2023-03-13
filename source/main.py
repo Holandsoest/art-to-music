37d39052348561f1f6359a59fcad317b8d997ad4
Maurice <mauricevanloenen@gmail.com>
2023-03-13 10:43:31 +0100

    SVM added

--- source/main.py
+++ source/main.py
@@ -2,18 +2,58 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-######### Template Matching ########
-img_rgb = cv.imread(r"C:\Users\mauri\Documents\ExampleShapes.png")
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread(r"C:\Users\mauri\Documents\BlueTriangle.png",0)
-w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
+################## Template Matching #############
+# img_rgb = cv.imread("res.png")
+# img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+# template = cv.imread(r"C:\Users\mauri\Documents\BlueTriangle.png",0)
+# w, h = template.shape[::-1]
+# res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+# threshold = 0.8
+# loc = np.where( res >= threshold)
+# for pt in zip(*loc[::-1]):
+#     cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
 
-cv.imwrite('res.png',img_rgb)
-cv.imshow('image', img_rgb)
-cv.waitKey(0)
+# cv.imwrite('res.png',img_rgb)
+# cv.imshow('image', img_rgb)
+# cv.waitKey(0)
+
+##################### SVM #######################
+# Set up training data
+labels = np.array([1, -1, -1, -1])
+trainingData = np.matrix([[501, 10], [255, 10], [501, 255], [10, 501]], dtype=np.float32)
+# Train the SVM
+svm = cv.ml.SVM_create()
+svm.setType(cv.ml.SVM_C_SVC)
+svm.setKernel(cv.ml.SVM_LINEAR)
+svm.setTermCriteria((cv.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
+svm.train(trainingData, cv.ml.ROW_SAMPLE, labels)
+# Data for visual representation
+width = 512
+height = 512
+image = np.zeros((height, width, 3), dtype=np.uint8)
+# Show the decision regions given by the SVM
+green = (0,255,0)
+blue = (255,0,0)
+for i in range(image.shape[0]):
+    for j in range(image.shape[1]):
+        sampleMat = np.matrix([[j,i]], dtype=np.float32)
+        response = svm.predict(sampleMat)[1]
+        if response == 1:
+            image[i,j] = green
+        elif response == -1:
+            image[i,j] = blue
+# Show the training data
+thickness = -1
+cv.circle(image, (501,  10), 5, (  0,   0,   0), thickness)
+cv.circle(image, (255,  10), 5, (255, 255, 255), thickness)
+cv.circle(image, (501, 255), 5, (255, 255, 255), thickness)
+cv.circle(image, ( 10, 501), 5, (255, 255, 255), thickness)
+# Show support vectors
+thickness = 2
+sv = svm.getUncompressedSupportVectors()
+for i in range(sv.shape[0]):
+    cv.circle(image, (int(sv[i,0]), int(sv[i,1])), 6, (128, 128, 128), thickness)
+cv.imwrite('result.png', image) # save the image
+cv.imshow('SVM Simple Example', image) # show it to the user
+cv.waitKey()
 
