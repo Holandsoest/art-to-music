8bbdc82ff4905ce9942171f9e3c53b511d4924c9
Giel Wouters <gielwouters2002@gmail.com>
2023-03-13 15:37:32 +0100

    adding classes and making a function of the midifile maker

--- test15.py
+++ test15.py
@@ -1,42 +1,55 @@
 from midiutil.MidiFile import MIDIFile
 import pygame
 
-def MakeSong(pitch, bpm, duration, volume, instrument, amount):
+def MakeSong(pitch, bpm, duration, volume, instrument, amount_of_instruments):
+
     # create MIDIFile object
-    midi = MIDIFile(1, removeDuplicates=False)
+    midi = MIDIFile(amount_of_instruments, removeDuplicates=False)
 
-    # add tracks
-    track1 = 0
     time = 0
-    channel = 0 
+    channel = 0
+    instruments = 0
 
-    # create track 1
-    midi.addTrackName(track1, time, "Track 1")
-    midi.addTempo(track1, time, bpm)
-    midi.addProgramChange(track1, channel, time, instrument)
+    while instruments < amount_of_instruments:
+    # create ass many tracks as instruments
+        midi.addTrackName(instruments, time, f"Track{instruments}")
+        midi.addTempo(instruments, time, bpm)
+        midi.addProgramChange(instruments, 0, time, instrument)
 
-    # track 1 notes
-    for i in range(4):
-        time = i * duration
-        midi.addNote(track1, channel, pitch, time, duration, volume)
-        midi.addNote(track1, channel, pitch+4, time, duration, volume)
-        midi.addNote(track1, channel, pitch+7, time, duration, volume)
-        time = i * duration
-        pitch = pitch + 1
+        for i in range(4):
+            time = i * duration
+            midi.addNote(instruments, channel, pitch, time, duration, volume)
+            midi.addNote(instruments, channel, pitch, time+2, duration, volume)
+            midi.addNote(instruments, channel, pitch, time+4, duration, volume)
+            time = i * duration
 
-    with open(f"output{amount}.mid", "wb") as output_file:
+        instruments +=1
+
+    with open("output.mid", "wb") as output_file:
         midi.writeFile(output_file)
+    pygame.mixer.init()
 
-MakeSong(pitch=60, bpm=120, duration=2, volume=255, instrument=4, amount=1)
-MakeSong(pitch=80, bpm=120, duration=2, volume=200, instrument=100, amount=2)
+    # load MIDI file
+    pygame.mixer.music.load("output.mid")
 
-mid1 = MIDIFile("output1.mid")
-mid2 = MIDIFile("output2.mid")
+    # play MIDI file
+    pygame.mixer.music.play()
 
-merged_mid = MIDIFile(2, removeDuplicates=False)
-# Copy time resolution. (If mid1 and mid2 have different
-# ticks_per_beat you will get timing problems.)
-merged_mid.ticks_per_beat = mid1.ticks_per_beat
+    # wait for music to finish playing
+    while pygame.mixer.music.get_busy():
+        continue
 
-merged_mid.tracks = mid1.tracks + mid2.tracks
-merged_mid.save('merged.mid')
+class object:
+    def __init__(self, pitch, bpm, duration, volume, instrument):
+        self.pitch = pitch
+        self.bpm = bpm
+        self.duration = duration
+        self.volume = volume
+        self.instrument = instrument
+
+p1 = object(60, 120, 2, 255, 50)
+p2 = object(50, 120, 2, 40, 30)
+p3 = object(25, 120, 2, 30, 40)
+amount_of_instruments = 1
+
+MakeSong(p1.pitch, p1.bpm, p1.duration, p1.volume, p1.instrument, amount_of_instruments)
