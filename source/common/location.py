6da78078af977ed8213360f01fd0a0a3ec95e72a
--global <holandsoest@gmail.com>
2023-03-14 13:21:47 +0100

    implement demo Pos class

--- source/common/location.py
+++ source/common/location.py
@@ -1,7 +1,33 @@
 class Pos:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-    def __str__(self):
+    """A simple class that can be used to store an x & y variables as a position or as a size."""
+
+    # Defining
+    def __init__(self, x=0, y=0, force_int=False):
+        if force_int:
+            self.x = int(x)
+            self.y = int(y)
+        else:
+            self.x = x
+            self.y = y
+    def __str__(self) -> str:
         return f'x:{self.x}, y:{self.y}'
     
+    # Basic math functions
+    def min(self) -> int | float:
+        """returns the minimum value from x or y"""
+        return min(self.x, self.y)
+    def max(self) -> int | float:
+        """returns the maximum value from x or y"""
+        return max(self.x, self.y)
+    def add(self) -> int | float:
+        """returns x + y"""
+        return self.x + self.y
+    def count(self) -> int | float:
+        """returns x * y"""
+        return self.x * self.y
+    
+def get_screensize() -> Pos:
+    """Uses tkinter to grab the screensize of the primary screen, returns a Pos class with the x and y values as length=x and hight=y"""
+    import tkinter
+    root = tkinter.Tk()
+    return Pos(x=root.winfo_screenwidth(), y=root.winfo_screenheight())
--- source/image_perspective.py
+++ source/image_perspective.py
@@ -1,11 +1,22 @@
+import common.location
 import cv2
 import os
 
 if __name__ == "__main__":
     print ('Running demo image_perspective')
-    absolute_path = os.path.join(os.getcwd(), 'haw.jpg')
-    img = cv2.imread('haw.jpg')
-    rows,cols,ch = img.shape
-    print(f'rows {rows},  cols {cols}, ch {ch}')
-    cv2.imshow('test',img)
-    input('hi')
+    absolute_path = os.path.join(os.getcwd(), 'source', 'haw.jpg')
+    print(absolute_path)
+    img = cv2.imread(absolute_path, 1)
+
+    # Get the sizes and crop the image if necessary
+    screen_size = common.location.get_screensize()
+    img_size = common.location.Pos(x=img.shape[1], y=img.shape[0])
+    crop_modifier = common.location.Pos(x=(screen_size.x / img_size.x), y=(screen_size.y / img_size.y))
+    print(f'Sizes:\n  Screen:\t{screen_size}\n  Original img:\t{img_size}')
+    print(f'  Cropped img:\tx:{int(img_size.x*crop_modifier.min())}, y:{int(img_size.y*crop_modifier.min())}, modifier:{crop_modifier.min()}')
+    cv2.imshow(f'Before',cv2.resize(img, (int(img_size.x * crop_modifier.min()),int(img_size.y * crop_modifier.min()))))
+
+    # Find the whiteboard coordinates
+    
+
+    cv2.waitKey(delay=0)
