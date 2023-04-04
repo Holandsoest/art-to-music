23c09bb5c60ed1300b48c9255284f147450bc964
Giel Wouters <gielwouters2002@gmail.com>
2023-04-04 18:15:28 +0200

    track open add on

--- Sound.py
+++ Sound.py
@@ -3,44 +3,65 @@ import numpy as np
 from scipy.io import wavfile
 from pydub import AudioSegment
 
-SAMPLE_RATE = 44100
-BUFFER_SIZE = 128
+import mido
 
-# Make an engine. We'll only need one.
-engine = daw.RenderEngine(SAMPLE_RATE, BUFFER_SIZE)
-synth = engine.make_plugin_processor("my_synth", r"C:\Program Files\Common Files\VST3\BBC Symphony Orchestra (64 Bit).vst3")
-#synth = engine.make_plugin_processor("my_synth", r"D:\Muziek\muziek\Fl studio plugins\Sylenth1\Sylenth1.dll")
-assert synth.get_name() == "my_synth"
-#synth.load_vst3_preset(r"C:\Users\gielw\Spitfire\Spitfire Audio - BBC Symphony Orchestra\Presets\v1.5.0\Discover Strings\BBCSODi_Violins 2_All.zpreset")
-instrument = "cello"
+# Open MIDI file
+midi = mido.MidiFile('MidiSound.mid')
 
-synth.load_state(instrument + "_preset")
+# Get the number of tracks in the MIDI file
+num_tracks = midi.numTracks()
 
-# For some plugins, it's possible to load presets:
-#print(synth.get_parameters_description())
-#synth.set_parameter("pitButton10")
-#synth.set_parameter("/MySine/freq", 440.)  # 440 Hz.
-synth.load_midi(r"MidiSound.mid", clear_previous=False, beats=False, all_events=False)
+# Loop through each track and print its contents
+for track_idx in range(num_tracks):
+    midi.trackIndex(track_idx)
+    print(f"Track {track_idx + 1}:")
+    
+    # Get the events in the track
+    events = midi.events()
+    
+    # Loop through each event and print its contents
+    for event in events:
+        print(event)
 
-# Plugins can show their UI.
-#synth.open_editor()  # Open the editor, make changes, and close
+def OpeningMidi():
+    SAMPLE_RATE = 44100
+    BUFFER_SIZE = 128
 
-#synth.save_state("flute_preset")
+    # Make an engine. We'll only need one.
+    engine = daw.RenderEngine(SAMPLE_RATE, BUFFER_SIZE)
+    synth = engine.make_plugin_processor("my_synth", r"C:\Program Files\Common Files\VST3\BBC Symphony Orchestra (64 Bit).vst3")
+    #synth = engine.make_plugin_processor("my_synth", r"D:\Muziek\muziek\Fl studio plugins\Sylenth1\Sylenth1.dll")
+    assert synth.get_name() == "my_synth"
+    #synth.load_vst3_preset(r"C:\Users\gielw\Spitfire\Spitfire Audio - BBC Symphony Orchestra\Presets\v1.5.0\Discover Strings\BBCSODi_Violins 2_All.zpreset")
+    instrument = "cello"
 
-engine.load_graph([
-                   (synth, [])
-])
+    synth.load_state(instrument + "_preset")
 
-# Load a MIDI file and keep the timing in units of beats. Changes to the Render Engine's BPM
-# will affect the timing.
+    # For some plugins, it's possible to load presets:
+    #print(synth.get_parameters_description())
+    #synth.set_parameter("pitButton10")
+    #synth.set_parameter("/MySine/freq", 440.)  # 440 Hz.
+    synth.load_midi(r"MidiSound.mid", clear_previous=False, beats=False, all_events=False)
 
-#print("synth num inputs: ", synth.get_num_input_channels())
-#print("synth num outputs: ", synth.get_num_output_channels())
+    # Plugins can show their UI.
+    #synth.open_editor()  # Open the editor, make changes, and close
 
-engine.set_bpm(120)
-engine.render(10)  
-audio = engine.get_audio()  
-wavfile.write('cello.wav', SAMPLE_RATE, audio.transpose()) # Don't forget to transpose!
+    #synth.save_state("flute_preset")
+
+    engine.load_graph([
+                    (synth, [])
+    ])
+
+    # Load a MIDI file and keep the timing in units of beats. Changes to the Render Engine's BPM
+    # will affect the timing.
+
+    #print("synth num inputs: ", synth.get_num_input_channels())
+    #print("synth num outputs: ", synth.get_num_output_channels())
+
+    engine.set_bpm(120)
+    engine.render(10)  
+    audio = engine.get_audio()  
+    wavfile.write('cello.wav', SAMPLE_RATE, audio.transpose()) # Don't forget to transpose!
 
 # Load the WAV file
 SAMPLE_RATE1, audio = wavfile.read('my_song2.wav')
