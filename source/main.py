2cbec135404fe56285614bb7e248b6ab10104396
Josha <joshakuipers@gmail.com>
2023-04-12 21:34:11 +0200

    work

--- source/main.py
+++ source/main.py
@@ -2,29 +2,44 @@ import cv2
 import image_processing as img_proc
 import common.midi_creation as mc
 import image_processing_ai as img_proc_ai
+from imageai.Detection import ObjectDetection
+
+#
+cap = cv2.VideoCapture(0)
+#
+
+
 
 if __name__ == "__main__":
-    img_path = 'files\image_processing\example_shapes (10).jpg'
-    img = cv2.imread(img_path)
 
-# img 14.jpg has 86 stars
-    # list_of_shapes, shapes = img_proc.readImage(img)
-    shapes = img_proc.get_contours_from_image(img)
+    obj_detect = ObjectDetection()
+    #obj_detect.setModelTypeAsYOLOv3()
+    obj_detect.setModelTypeAsTinyYOLOv3()
+    obj_detect.setModelPath(r"D:\BeCreative\Music based on Art\shape\tiny-yolov3.pt")
+    obj_detect.loadModel()
 
-    img_proc_ai.detect_shapes_with_contour(shapes, img)
-
-    img_proc_ai.detect_shapes_with_ai(img)
-
-    print('Press esc to continue...')   
 
     while(1):
-        cv2.imshow("image",img)
+        
+
+        ret, img = cap.read()
+        imgcamm, preds = obj_detect.detectObjectsFromImage(input_image=img, output_type="array",
+                                                                   display_percentage_probability=False,
+                                                                   display_object_name=True)
+        shapes = img_proc.get_contours_from_image(imgcamm)
+        img_proc_ai.detect_shapes_with_contour(shapes, imgcamm)
+        img_proc_ai.detect_shapes_with_ai(imgcamm)
+
+        _, imageFrame = cap.read()
+        cv2.imshow("ai", imgcamm)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break    
 
         # Break the loop when user hits 'esc' key 
-        if cv2.waitKey(20) & 0xFF ==27:
-            print('Enjoy the music')
-            break
+        # if cv2.waitKey(20) & 0xFF ==27:
+        #     print('Enjoy the music')
+        #     break
         
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
     # mc.MakeSong(list_of_shapes)
