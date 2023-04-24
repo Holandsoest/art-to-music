7f6c6f2e5ee999ac031d3e8eb751265a8088a5d1
BramvanLimpt <b.vanlimpt@teampi.nl>
2023-04-24 15:22:57 +0200

    Extra changes midi creation

--- source/common/midi_creation.py
+++ source/common/midi_creation.py
@@ -3,6 +3,14 @@ from midiutil.MidiFile import *
 import os
 
 def MakeSong(list):
+
+    def note (pitch, low, high):
+        if pitch in forbidden_notes:
+            if abs(pitch - low) < abs(pitch - high):
+                return pitch + 1 #value closer to low limit so + value
+            else:
+                return pitch - 1 #value closer to low limit so - value
+
     #pitch, bpm, duration, volume, instrument, amount
     amount_of_instruments = len(list) # number of object on the screen 
     model_custom_path = os.path.join(os.getcwd(), 'files', 'audio_generator', 'midi_files')
@@ -85,17 +93,9 @@ def MakeSong(list):
         #add drum notes
         if amount_of_drum > 0 and shape.instrument == "drum":
             
-            #check for forbidden notes between 74 and 96
-            drum_pitch = shape.pitch
-            print (drum_pitch)
-            drum_low = 74
-            drum_high = 96
-            if drum_pitch in forbidden_notes:
-                if abs(drum_low - drum_pitch) < abs(drum_pitch - drum_high):
-                    drum_pitch = drum_pitch + 1 #value closer to low limit so + value
-                else:
-                    drum_pitch = drum_pitch - 1 #value closer to low limit so - value
-            print (drum_pitch)
+            #check for forbidden notes between 69 and 86
+            drum_pitch = note(shape.pitch, 69, 86)
+
             #choose melodie
             if shape.duration == 1: #Q1
                 melodie = melodie5
@@ -127,73 +127,141 @@ def MakeSong(list):
 
         #add guitar notes
         if amount_of_guitar > 0 and shape.instrument == "guitar":
-            # add tracks
-            time_guitar = 2 # time to zero
-            channel_guitar = 0 #channel to zero
+            
+            #check for forbidden notes between 74 and 96
+            guitar_pitch = note(shape.pitch, 74, 96)
 
-            guitar_pitch = shape.pitch
-            for shape.pitch in forbidden_notes:
-                guitar_pitch =  guitar_pitch + 1
+            #choose melodie
+            if shape.duration == 1: #Q1
+                melodie = melodie5
+            elif shape.duration == 2: #Q2
+                melodie = melodie6
+            elif shape.duration == 3: #Q3
+                melodie = melodie7
+            elif shape.duration == 4: #Q4
+                melodie = melodie8
+            
+            # add tracks
+            time_guitar = 0 # time to zero
+            channel_guitar = 0 #channel to zero
 
             # create ass many tracks as objects on the board    
             midi_guitar.addTrackName(object_guitar, time_guitar, f"Track{object_guitar}") # give track a name
             midi_guitar.addTempo(object_guitar, time_guitar, shape.bpm) # set bpm
             midi_guitar.addProgramChange(object_guitar, 0, time_guitar, 1) # add instrument = shape.instrument = 1
-            midi_guitar.addNote(object_guitar, channel_guitar, guitar_pitch, time_guitar, shape.duration, shape.volume) # make a note
+            #add melodie nodes to track
+            i = 0
+            for i in range(0,4):
+                print(i)
+                print(melodie[i])
+                if melodie[i] >= 1:
+                    midi_drum.addNote(object_guitar, channel_guitar, guitar_pitch, time_guitar + melodie[i], shape.duration, shape.volume) # make a note
+                i += 1
 
             object_guitar +=1
 
         #add flute notes
         if amount_of_flute > 0 and shape.instrument == "flute":
-            # add tracks
-            time_flute = 4 # time to zero
-            channel_flute = 0 #channel to zero
+            
+            #check for forbidden notes between 72 and 108
+            flute_pitch = note(shape.pitch, 72, 108)
 
-            flute_pitch = shape.pitch
-            for shape.pitch in forbidden_notes:
-                flute_pitch = flute_pitch + 1
+            #choose melodie
+            if shape.duration == 1: #Q1
+                melodie = melodie5
+            elif shape.duration == 2: #Q2
+                melodie = melodie6
+            elif shape.duration == 3: #Q3
+                melodie = melodie7
+            elif shape.duration == 4: #Q4
+                melodie = melodie8
+            
+            # add tracks
+            time_flute = 0 # time to zero
+            channel_flute = 0 #channel to zero
 
             # create ass many tracks as objects on the board    
             midi_flute.addTrackName(object_flute, time_flute, f"Track{object_flute}") # give track a name
             midi_flute.addTempo(object_flute, time_flute, shape.bpm) # set bpm
             midi_flute.addProgramChange(object_flute, 0, time_flute, 1) # add instrument = shape.instrument = 1
-            midi_flute.addNote(object_flute, channel_flute, flute_pitch, time_flute, shape.duration, shape.volume) # make a note
+            #add melodie nodes to track
+            i = 0
+            for i in range(0,4):
+                print(i)
+                print(melodie[i])
+                if melodie[i] >= 1:
+                    midi_drum.addNote(object_flute, channel_flute, flute_pitch, time_flute + melodie[i], shape.duration, shape.volume) # make a note
+                i += 1
 
             object_flute +=1
 
         #add violin notes
         if amount_of_violin > 0 and shape.instrument == "violin":
-            # add tracks
-            time_violin = 6 # time to zero
-            channel_violin = 0 #channel to zero
+            
+            #check for forbidden notes between 76 and 103
+            violin_pitch = note(shape.pitch, 76, 103)
 
-            violin_pitch = shape.pitch
-            for shape.pitch in forbidden_notes:
-                violin_pitch = violin_pitch + 1
+            #choose melodie
+            if shape.duration == 1: #Q1
+                melodie = melodie5
+            elif shape.duration == 2: #Q2
+                melodie = melodie6
+            elif shape.duration == 3: #Q3
+                melodie = melodie7
+            elif shape.duration == 4: #Q4
+                melodie = melodie8
+                        
+            # add tracks
+            time_violin = 0 # time to zero
+            channel_violin = 0 #channel to zero
 
             # create ass many tracks as objects on the board    
             midi_violin.addTrackName(object_violin, time_violin, f"Track{object_violin}") # give track a name
             midi_violin.addTempo(object_violin, time_violin, shape.bpm) # set bpm
             midi_violin.addProgramChange(object_violin, 0, time_violin, 1) # add instrument = shape.instrument = 1
-            midi_violin.addNote(object_violin, channel_violin, violin_pitch, time_violin, shape.duration, shape.volume) # make a note
-
+            #add melodie nodes to track
+            i = 0
+            for i in range(0,4):
+                print(i)
+                print(melodie[i])
+                if melodie[i] >= 1:
+                    midi_drum.addNote(object_violin, channel_violin, violin_pitch, time_violin + melodie[i], shape.duration, shape.volume) # make a note
+                i += 1
+                
             object_violin +=1
         
         #add cello notes
         if amount_of_cello > 0 and shape.instrument == "cello":
+            
+            #check for forbidden notes between 48 and 77
+            cello_pitch = note(shape.pitch, 48, 77)
+
+            #choose melodie
+            if shape.duration == 1: #Q1
+                melodie = melodie5
+            elif shape.duration == 2: #Q2
+                melodie = melodie6
+            elif shape.duration == 3: #Q3
+                melodie = melodie7
+            elif shape.duration == 4: #Q4
+                melodie = melodie8
+                        
             # add tracks
             time_cello = 8 # time to zero
             channel_cello = 0 #channel to zero
-
-            cello_pitch = shape.pitch
-            for shape.pitch in forbidden_notes:
-                cello_pitch = cello_pitch + 1
 
             # create ass many tracks as objects on the board    
             midi_cello.addTrackName(object_cello, time_cello, f"Track{object_cello}") # give track a name
             midi_cello.addTempo(object_cello, time_cello, shape.bpm) # set bpm
             midi_cello.addProgramChange(object_cello, 0, time_cello, 1) # add instrument = shape.instrument = 1
-            midi_cello.addNote(object_cello, channel_cello, cello_pitch, time_cello, shape.duration, shape.volume) # make a note
+            #add melodie nodes to track
+            i = 0
+            for i in range(0,4):
+                print(i)
+                print(melodie[i])
+                if melodie[i] >= 1:
+                    midi_drum.addNote(object_cello, channel_cello, cello_pitch, time_cello + melodie[i], shape.duration, shape.volume) # make a note
+                i += 1
 
             object_cello +=1
         
@@ -212,3 +280,5 @@ def MakeSong(list):
                 midi_cello.writeFile(output_file5)
             
         iteration += 1
+
+
