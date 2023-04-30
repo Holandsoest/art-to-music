885409e8980040ad27f47ec0bba143e05e91f532
Josha <joshakuipers@gmail.com>
2023-04-30 17:18:33 +0200

    ok

--- source/main.py
+++ source/main.py
@@ -12,3 +12,5 @@ my_label1 = Label(root, text = 'hello ').pack()
 root.iconbitmap('triangle.ico')
 
 root.mainloop()
+
+https://www.google.com/search?q=gui+maker+python+with+vscode+with+simbols&biw=1536&bih=722&tbm=vid&sxsrf=AJOqlzUqRIukOTT_tU1AtpEtFzobwnsM7Q%3A1676552355654&ei=oyjuY6HNJ-qPi-gP26io8AI&ved=0ahUKEwjhquycjJr9AhXqxwIHHVsUCi4Q4dUDCA0&uact=5&oq=gui+maker+python+with+vscode+with+simbols&gs_lcp=Cg1nd3Mtd2l6LXZpZGVvEAMyBwghEKABEAoyBwghEKABEAo6BAgjECc6BQghEKABOgsIIRAWEB4Q8QQQHVDrCVjrKWDsKmgAcAB4AIAB3wGIAYMKkgEGMTIuMS4xmAEAoAEBwAEB&sclient=gws-wiz-video#fpstate=ive&vld=cid:8ada7cec,vid:zZ0r3Ja3mEA
