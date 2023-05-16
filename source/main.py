2bc61cb6bb8cdb3483ace72ff981de4083bce8de
Kirill Peljuh <445131@student.fontys.nl>
2023-05-16 16:12:45 +0200

    Fix

--- source/main.py
+++ source/main.py
@@ -1,15 +1,19 @@
 import cv2
 import image_processing as img_proc
-import common.midi_creation as midi_creation
-import common.midi_processing as midi_processing
+#import common.midi_creation as midi_creation
+#import common.midi_processing as midi_processing
 import image_processing_ai as img_proc_ai
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 img_proc_ai.setup_ai()
 
 if __name__ == "__main__":
     while(1):
-        success, img = cap.read()
+        img_counter = 0
+        img_path = 'files\\image_processing\\test_shapes_with_camera0.png'
+        img_path = 'files\image_processing\opencv_fram_5.png'
+        img = cv2.imread(img_path)
+        assert img is not None, "file could not be read, check with os.path.exists()"
         image_ai, list_of_shapes = img_proc_ai.detect_shapes_with_ai(img)
         cv2.imshow("ai", image_ai)
 
