e9b181dd7a6a54f5dd686437b59216cdcb34ad84
jetson <realfake-email@adress>
2023-06-06 14:02:54 +0200

    reorder

--- source/main.py
+++ source/main.py
@@ -1,39 +1,24 @@
 import cv2
-import gi
-gi.require_version('Gst', '1.0')
-from gi.repository import Gst
 import numpy as np
-#import image_processing as img_proc
-#import common.midi_creation as midi_creation
-#import common.midi_processing as midi_processing
+import common.midi_creation as midi_creation
+import common.midi_processing as midi_processing
 import image_processing_ai as img_proc_ai
 import take_image 
 
 img_proc_ai.setup_ai()
 
 if __name__ == "__main__":
+    # TODO: Do this when jetson button press
+
     img = take_image.get_image()
+    cv2.imshow('Camera', img)
+
     image_ai, list_of_shapes = img_proc_ai.detect_shapes_with_ai(img)
 
-    cv2.imshow('Camera', img)
+    midi_creation.MakeSong(list_of_shapes)
+    midi_processing.AudiRenderPlugin(list_of_shapes)
+
+    # TODO: play audio with pygame
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-    # if __name__ == "__main__":
-        
-    #     while(1):
-    #         img = camera.read()
-    #         status = camera.hasError()
-    #         print(status)
-    #         #success, img = cap.read()
-    
-
-    #         # img_proc.display_list_of_shapes(list_of_shapes)
-    #         # midi_creation.MakeSong(list_of_shapes)
-    #         # midi_processing.AudiRenderPlugin(list_of_shapes)
-
-    #         if cv2.waitKey(20) & 0xFF == ord('q'):
-    #             break
-    #cap.release()
-   # cv2.destroyAllWindows()
