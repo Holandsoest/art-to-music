a525375e6844987fd53ab5423097af89efb771bf
jetson <realfake-email@adress>
2023-06-06 13:30:03 +0200

    unknown wip

--- source/main.py
+++ source/main.py
@@ -1,20 +1,20 @@
 import cv2
+#print(cv2.__version__)
 import image_processing as img_proc
+# import common.midi_creation as midi_creation
+# import common.midi_processing as midi_processing
 #import common.midi_creation as midi_creation
 #import common.midi_processing as midi_processing
 import image_processing_ai as img_proc_ai
 
-#cap = cv2.VideoCapture(0)
-img_proc_ai.setup_ai()
+
+#cap = cv2.VideoCapture('nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink',cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(0)
+#img_proc_ai.setup_ai()
 
 if __name__ == "__main__":
     while(1):
-        #change
-        img_counter = 0
-        img_path = 'files\\image_processing\\test_shapes_with_camera0.png'
-        img_path = 'files\image_processing\opencv_fram_5.png'
-        img = cv2.imread(img_path)
-        assert img is not None, "file could not be read, check with os.path.exists()"
+        success, img = cap.read()
         image_ai, list_of_shapes = img_proc_ai.detect_shapes_with_ai(img)
         cv2.imshow("ai", image_ai)
 
