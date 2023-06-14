a5282f9a4bf8a21de9acf5749f1012597a9f12ae
Holandsoest <103452406+Holandsoest@users.noreply.github.com>
2023-06-14 09:25:12 +0200

    Implement music loop busy wait

--- source/common/midi_processing.py
+++ source/common/midi_processing.py
@@ -2,6 +2,8 @@ import dawdreamer as daw
 from scipy.io import wavfile
 from pydub import AudioSegment
 from pygame import mixer
+import typing
+import cv2
 import time
 import os
 
@@ -62,3 +64,31 @@ def audio_rendering(bpm):
     # Export the combined audio to an MP3 file
     with open(os.path.join('files','audio_generator','created_song.mp3'), "wb") as output_file1:
                 combined_sound6.export(output_file1, format = "mp3")
+
+def play_loop(song_absolute_path, decay=0.75, cutoff=0.1) -> typing.Any:
+    """Uses pygame to play the given music in a blocking manner.  
+
+    Every time it plays the volume decays with the `decay`until it reaches a certain `cutoff` value.
+    
+    If duding playing you interrupted it with a keystroke it returns the `cv2.type`"""
+    mixer.init()
+    mixer.music.load(song_absolute_path)
+    volume = 1.0
+    
+    while volume > cutoff:
+        mixer.music.set_volume(volume)
+        mixer.music.play()
+        print(f'Playing music at {volume*100}% volume')
+        while mixer.music.get_busy():
+            key = cv2.waitKey(1) # time in ms before polling again
+            if key != -1:
+                mixer.stop()
+                mixer.quit()
+                return key # `-1` is time expired
+        volume = volume * decay
+    mixer.quit()
+    print('Volume cutoff')
+    return -1
+
+if __name__ == "__main__":
+    audio_rendering()
--- source/main.py
+++ source/main.py
@@ -12,38 +12,43 @@ img_proc_ai.setup_ai()
 import os
 import time
 if __name__ == "__main__":
-    # TODO: Do this when jetson button press
+    while True:
     
-    img = take_image.get_image()
-    cv2.imshow('Camera', img)
+        # BUG: currently there is a problem when there are no shapes detected TODO: change this back #img = take_image.get_image()
+        img_path = os.path.join(os.getcwd(), 'files','image_processing','example_shapes (3).png')
+        img = cv2.imread(img_path)
+        cv2.imshow('Camera', img)
 
-    image_ai, list_of_shapes = img_proc_ai.detect_shapes_with_ai(img)
-    img_proc.display_list_of_shapes(list_of_shapes)
+        image_ai, list_of_shapes = img_proc_ai.detect_shapes_with_ai(img)
+        img_proc.display_list_of_shapes(list_of_shapes)
 
-    bpm = midi_creation.MakeSong(list_of_shapes) 
-    
-    processes = [
-        mp.Process(target=midi_processing.instrument, args=(bpm, "drum")),
-        mp.Process(target=midi_processing.instrument, args=(bpm, "violin")),
-        mp.Process(target=midi_processing.instrument, args=(bpm, "guitar")),
-        mp.Process(target=midi_processing.instrument, args=(bpm, "flute")),
-        mp.Process(target=midi_processing.instrument, args=(bpm, "saxophone")),
-        mp.Process(target=midi_processing.instrument, args=(bpm, "clap")),
-        mp.Process(target=midi_processing.instrument, args=(bpm, "piano"))
-    ]
-    
-    # # Start all processes
-    # for process in processes:
-    #     process.start()
-
-    # # Wait for all processes to finish
-    # for process in processes:
-    #     process.join()
+        bpm = midi_creation.MakeSong(list_of_shapes) 
         
-    midi_processing.audio_rendering(bpm)
+        processes = [
+            mp.Process(target=midi_processing.instrument, args=(bpm, "drum")),
+            mp.Process(target=midi_processing.instrument, args=(bpm, "violin")),
+            mp.Process(target=midi_processing.instrument, args=(bpm, "guitar")),
+            mp.Process(target=midi_processing.instrument, args=(bpm, "flute")),
+            mp.Process(target=midi_processing.instrument, args=(bpm, "saxophone")),
+            mp.Process(target=midi_processing.instrument, args=(bpm, "clap")),
+            mp.Process(target=midi_processing.instrument, args=(bpm, "piano"))
+        ]
+        
+        # Start all processes
+        for process in processes:
+            process.start()
 
-    # TODO: play audio with pygame
+        # Wait for all processes to finish
+        for process in processes:
+            process.join()
+            
+        midi_processing.audio_rendering(bpm)
 
-    cv2.waitKey(0)
+        key = midi_processing.play_loop(os.path.join(os.getcwd(), 'files', 'audio_generator', 'created_song.mp3'),
+                                                    decay= 0.75,
+                                                    cutoff=0.05)
+        if key == -1: 
+            key = cv2.waitKey(0)
+        if key == ord('q'): break
     cv2.destroyAllWindows()
 
